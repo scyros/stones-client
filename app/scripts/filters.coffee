7e1e394a0a0b17f53231e9deb699f1ec9343461d
Carlos León <carlos.eduardo.leon.franco@gmail.com>
2014-02-26 16:20:10 +0100

    Granted styles inside stTriState

--- app/scripts/filters.coffee
+++ app/scripts/filters.coffee
@@ -1,13 +1,16 @@
 'use strict'
 
 angular.module('stones')
-  .filter 'stTriState', () ->
-    (input) ->
-      out = ''
-      if input is true
-        out += '<i class="fa fa-check-circle" style="color:green;"></i> '
-      else if input is false
-        out += '<i class="fa fa-times-circle" style="color:red;"></i> '
-      else
-        out += '<i class="fa fa-question-circle" style="color:yellow;"></i> '
-      out
+  .filter 'stTriState', [
+    '$sce',
+    ($sce) ->
+      (input) ->
+        out = ''
+        if input is true
+          out += '<i class="fa fa-check-circle" style="color:green;"></i> '
+        else if input is false
+          out += '<i class="fa fa-times-circle" style="color:red;"></i> '
+        else
+          out += '<i class="fa fa-question-circle" style="color:yellow;"></i> '
+        $sce.trustAsHtml(out)
+    ]
--- dist/scripts/stones.js
+++ dist/scripts/stones.js
@@ -149,20 +149,22 @@ automate and standarize client-server communications.
 */
 (function() {
   'use strict';
-  angular.module('stones').filter('stTriState', function() {
-    return function(input) {
-      var out;
-      out = '';
-      if (input === true) {
-        out += '<i class="fa fa-check-circle" style="color:green;"></i> ';
-      } else if (input === false) {
-        out += '<i class="fa fa-times-circle" style="color:red;"></i> ';
-      } else {
-        out += '<i class="fa fa-question-circle" style="color:yellow;"></i> ';
-      }
-      return out;
-    };
-  });
+  angular.module('stones').filter('stTriState', [
+    '$sce', function($sce) {
+      return function(input) {
+        var out;
+        out = '';
+        if (input === true) {
+          out += '<i class="fa fa-check-circle" style="color:green;"></i> ';
+        } else if (input === false) {
+          out += '<i class="fa fa-times-circle" style="color:red;"></i> ';
+        } else {
+          out += '<i class="fa fa-question-circle" style="color:yellow;"></i> ';
+        }
+        return $sce.trustAsHtml(out);
+      };
+    }
+  ]);
 
 }).call(this);
 
